f97a6488ec7b717e0fd28c9a2437eb627e6f6307
Prasanth P <prasanth@gmail.com>
2023-05-30 20:34:46 +0530

    update

--- HomeRent/settings.py
+++ HomeRent/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://dream-home-rentals.onrender.com','localhost','127.0.0.1']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
